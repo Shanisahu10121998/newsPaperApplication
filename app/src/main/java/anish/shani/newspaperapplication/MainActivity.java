01d0c22e5c070fd4c48fbe17f228612ea43091a6
Shani sahu <shanisahu1012@gmail.com>
2019-09-21 14:27:59 +0530

    SecondPush

--- app/src/main/java/anish/shani/newspaperapplication/MainActivity.java
+++ app/src/main/java/anish/shani/newspaperapplication/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -14,21 +15,30 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<HashMap<String, String>> arrayListNews;
+    RecyclerView.Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // initDrawerAnd ToolBar
+        // initDrawerAndToolbar
         initComponants();
     }
 
     private void initComponants() {
         mRecyclerView = findViewById(R.id.mRecyclerView);
-        mLayoutManager=new LinearLayoutManager(MainActivity.this);
+        mLayoutManager = new LinearLayoutManager(MainActivity.this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         callAPI();
@@ -37,12 +47,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void callAPI() {
 
-
-// ...
-
 // Instantiate the RequestQueue.
         RequestQueue queue = Volley.newRequestQueue(this);
-        String url ="http://www.google.com";
+        String url = "https://api.myjson.com/bins/9gcnx";
 
 // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
@@ -50,7 +57,8 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-
+                        parseAPIResonse(response);
+                        Toast.makeText(MainActivity.this, "Printing The Response" + response, Toast.LENGTH_SHORT).show();
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -61,5 +69,34 @@ public class MainActivity extends AppCompatActivity {
 
 // Add the request to the RequestQueue.
         queue.add(stringRequest);
+    }
+
+    private void parseAPIResonse(String response) {
+
+        try {
+            JSONObject objResponse = new JSONObject(response);
+            JSONArray arrayHeadlines = objResponse.getJSONArray("headlines");
+            arrayListNews = new ArrayList<>();
+            for (int i=0; i< arrayHeadlines.length();i++){
+                JSONObject objItem =arrayHeadlines.getJSONObject(i);
+                String title=objItem.getString("title");
+                String imgUrl=objItem.getString("imgUrl");
+                String description=objItem.getString("description");
+
+                HashMap<String,String> map=new HashMap<>();
+                map.put("title",title);
+                map.put("url",imgUrl);
+                map.put("detail",description);
+                arrayListNews.add(map);
+
+          //set Adapter
+                mAdapter=new  HomeListAdapter(MainActivity.this,arrayListNews);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
